8c2c0c922fa1f905d1c7b9011c79659c16e8ed84
nandasgithub <nandakishore.rddy@gmail.com>
2014-12-17 16:29:05 +0530

    Adding Bash Script file which has all updates and downloads when installed Ubuntu

---
+++ install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+
+# for update and upgrade
+echo " <---------------------------------------------  UPDATING AND UPGRADING  --------------------------------------------->"
+sudo apt-get update && sudo apt-get upgrade
+
+# installing vim
+echo " <---------------------------------------------  INSTALLING VIM  --------------------------------------------->"
+sudo apt-get install vim
+
+# installing flash player
+echo " <---------------------------------------------  INSTALLING FLASH PLAYER  --------------------------------------------->"
+sudo apt-get install flashplugin-installer
+
+#installing vlc player
+echo " <---------------------------------------------  INSTALLING VLC PLAYER  --------------------------------------------->"
+
+# for setting .vimrc file
+echo "set nu" >> .vimrc
+echo "set cindent" >> .vimrc
+echo "set autoindnet" >> .vimrc
+
+sudo apt-get update && sudo apt-get upgrade
+# installing chrome
+echo " <---------------------------------------------  INSTALLING CHROME  --------------------------------------------->"
+
+if [ $(uname -m) == "x86_64" ]
+then
+	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb	
+	sudo apt-get -f install
+	sudo dpkg -i google-chrome-stable_current_amd64.deb
+else
+	wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb
+	sudo apt-get -f install
+	sudo dpkg -i google-chrome-stable_current_i386.deb
+fi
+sudo apt-get -f install
+
+sudo apt-get update && sudo apt-get upgrade
+# installing skype
+echo " <---------------------------------------------  INSTALLING SKYPE  --------------------------------------------->"
+
+if [ $(uname -m) == "x86_64" ]
+then
+	wget http://www.skype.com/go/getskype-linux-beta-ubuntu-64
+else
+	wget http://www.skype.com/go/getskype-linux-beta-ubuntu-32
+fi
+sudo apt-get -f install
+sudo dpkg -i getskype-*
+sudo apt-get -f install
